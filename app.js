de164b62583ddce042b02a5f46b64816d6dc444a
Oscar Acuña <oscar.gustavo.pilares99@gmail.com>
2023-02-22 17:23:19 -0600

    Introducción en TypeScript

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+const msg = 'Hola Mundo';
+const nuevoMsg = 'Hola Mundo!!';
+const hero = {
+    name: 'Iroman',
+    age: 445
+};
+console.log(nuevoMsg);
+console.log(hero);
+const vengadores = {
+    nombre: 'Vengadores',
+    edad: 25
+};
+vengadores.edad = 50;
+console.log(vengadores.edad + 1);
